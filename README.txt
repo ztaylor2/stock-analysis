b27a7f7d06566e7d2be26151c69d50cb38d9bd3b
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2017-11-12 12:51:49 -0800

    Basic pyramid file structure built with cookie cutter.

---
+++ README.txt
@@ -0,0 +1,33 @@
+stock_analysis
+==============
+
+Getting Started
+---------------
+
+- Change directory into your newly created project.
+
+    cd stock_analysis
+
+- Create a Python virtual environment.
+
+    python3 -m venv env
+
+- Upgrade packaging tools.
+
+    env/bin/pip install --upgrade pip setuptools
+
+- Install the project in editable mode with its testing requirements.
+
+    env/bin/pip install -e ".[testing]"
+
+- Configure the database.
+
+    env/bin/initialize_stock_analysis_db development.ini
+
+- Run your project's tests.
+
+    env/bin/pytest
+
+- Run your project.
+
+    env/bin/pserve development.ini
---
+++ setup.py
@@ -0,0 +1,68 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'plaster_pastedeploy',
+    'pyramid >= 1.9a',
+    'pyramid_debugtoolbar',
+    'pyramid_jinja2',
+    'pyramid_retry',
+    'pyramid_tm',
+    'ipython',
+    'pyramid_ipython',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'ipython',
+    'pandas',
+    'numpy',
+    'scikit-learn',
+    'matplotlib',
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',
+    'pytest-cov',
+    'tox',
+]
+
+setup(
+    name='Stock Analysis',
+    version='0.0',
+    description='Stock analysis application written in python',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = stock_analysis:main',
+        ],
+        'console_scripts': [
+            'initdb = stock_analysis.scripts.initializedb:main',
+        ],
+    },
+)
